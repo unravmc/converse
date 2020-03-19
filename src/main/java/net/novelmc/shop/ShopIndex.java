6d2aac3c8565f5983b424d5ae19ddb45613f98af
Paul Reilly <pawereus@gmail.com>
2020-03-19 17:10:46 -0400

    fixed an oopsie using @NonNull, removed it entirely. should be fine, right?

--- src/main/java/net/novelmc/shop/ShopIndex.java
+++ src/main/java/net/novelmc/shop/ShopIndex.java
@@ -1,15 +1,11 @@
 package net.novelmc.shop;
 
-import com.sun.istack.internal.NotNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -18,11 +14,11 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class ShopIndex implements InventoryHolder, Listener {
     private final Inventory INV;
     
+    @SuppressWarnings("")
     public ShopIndex() {
         INV = Bukkit.createInventory(this, 9, "UnraveledMC Shop");
     }
     
-    @NotNull
     @Override
     public Inventory getInventory() {
         return INV;
@@ -33,6 +29,7 @@ public class ShopIndex implements InventoryHolder, Listener {
         INV.addItem(createGuiItem(Material.DIAMOND, "Option 1", "Lore Line 1", "Lore Line 2"));
     }
     // Create new gui item in inventory;
+    @SuppressWarnings("")
     private ItemStack createGuiItem(Material mat, String name, String...lore) {
         ItemStack item = new ItemStack(mat, 1);
         ItemMeta meta = item.getItemMeta();
