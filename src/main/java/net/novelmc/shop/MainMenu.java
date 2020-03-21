ab99fee22f53a0641054fbd1d044162a4bae5b20
Paul Reilly <pawereus@gmail.com>
2020-03-21 15:54:17 -0400

    changed name for test shop
    Changed over the entire shop to an abstract type of gui calling. This should allow us to create and store gui's on the fly using commands. Soon to be added is the coins and everything else; this is just to make sure the shop runs properly first.

--- src/main/java/net/novelmc/shop/MainMenu.java
+++ src/main/java/net/novelmc/shop/MainMenu.java
@@ -5,7 +5,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class MainMenu extends ShopIndex {
     public MainMenu() {
-        super(9, "");
+        super(9, "UnraveledMC Shop");
         
         setItem(1, new ItemStack(Material.STICK), player -> {
             player.sendMessage("This is a test.");
